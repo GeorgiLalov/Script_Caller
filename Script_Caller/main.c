7544fa3d734b2fc13fa5fa59652f881209265d6b
GeorgiLalov <lalov.georgi.l@gmail.com>
2017-06-13 21:40:32 +0300

    Add everything by now

---
+++ Script_Caller/main.c
@@ -0,0 +1,191 @@
+/**
+* @about Calling scripts at a specific time.
+*
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <pthread.h>
+
+//! The folder where are the scripts.
+#define SCRIPTS_FOLDER    "scripts/"
+
+//! The key word for calling python scripts from the console.
+#define PYTHON_COMMAND    "python "
+
+//! TODO: Calculate them.
+#define SCRIPT_NAME_MAX_LENGTH    100
+#define BUFFER_NAME_MAX_LENGTH    120
+
+/* FILESCOPE VARIABLES */
+
+//! Holding needed information about the script.
+typedef struct
+{
+    int       hour;
+    int       minute;
+    int       second;
+    char      script_name[SCRIPT_NAME_MAX_LENGTH];
+    pthread_t *pthread_id;
+} tsScriptInfo;
+
+/* THREAD IDS */
+
+pthread_t backupMonitor_pthread;
+
+/* SCRIPTS FOR EXECUTION */
+
+static tsScriptInfo fs_scripts_for_execution[] =
+{
+    // hour, minute, second, script_name,         pthread_id
+    {  0,    0,    0,       "backup_monitor.py", &backupMonitor_pthread}
+};
+
+//! Count of the scripts.
+static int fs_count_of_scripts_for_execution = (sizeof(fs_scripts_for_execution) / sizeof(tsScriptInfo));
+
+/* FUNCTION PROTOTYPES */
+
+void *phread_start_point(void* fp_argv);
+void create_new_thread(const char *fp_script_name,
+                       pthread_t  *fp_pthread_id);
+void monitor_time(const struct tm *fp_time_info);
+
+/* FILE SCOPE FUNCTIONS */
+
+/**
+ * @brief Main Entry Point
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char *argv[])
+{
+    time_t    rawtime;
+    struct tm *timeinfo = NULL;
+    //struct tm timeinfo = {1, 10, 12, 0, 0, 0, 0, 0, 0, 0, NULL};
+    //monitor_time(&timeinfo);
+
+    //! Infinite loop.
+    while (1)
+    {
+        //! Getting current time.
+        time (&rawtime);
+        timeinfo = localtime (&rawtime);
+
+        //! Check for needed call to script.
+        monitor_time(timeinfo);
+    }
+
+    return 0;
+}
+
+/**
+ * @brief Start point of a new thread.
+ *        Every script is called by different thread, because system() function
+ *        is waiting the script to end.
+ * @param fp_argv    The script name.
+ * @return NULL
+ */
+void *phread_start_point(void* fp_argv)
+{
+    char *script_name = (char*) fp_argv;
+    char ex_command_buffer[BUFFER_NAME_MAX_LENGTH] = { '\0' };
+
+    if (script_name == NULL)
+    {
+        printf("script_name is NULL\n");
+
+        exit(1);
+    }
+    else
+    {
+        //! Create the full command that is going to be printed on the console.
+        //! Should be something like: "python scripts/backup_monitor.py"
+        strcat(ex_command_buffer, PYTHON_COMMAND);
+        strcat(ex_command_buffer, SCRIPTS_FOLDER);
+        strcat(ex_command_buffer, script_name);
+
+        printf("Executing script: %s\n", ex_command_buffer);
+
+        //! TODO: Check ret value for error.
+        system(ex_command_buffer);
+    }
+
+    return NULL;
+}
+
+/**
+ * @brief Creating a new thread for a script.
+ * @param fp_script_name    The name of the script.
+ * @param fp_pthread_id     The ID of the thread.
+ */
+void create_new_thread(const char *fp_script_name,
+                       pthread_t  *fp_pthread_id)
+{
+    if (fp_script_name == NULL)
+    {
+        printf("fp_script_name is NULL\n");
+
+        exit(1);
+    }
+    else if (fp_pthread_id == NULL)
+    {
+        printf("fp_pthread_id is NULL\n");
+
+        exit(1);
+    }
+    else
+    {
+        //! Create the new thread.
+        pthread_create(fp_pthread_id, NULL,
+                       phread_start_point, (void *) fp_script_name);
+    }
+}
+
+/**
+ * @brief Every one second check if some script needs to be runned and
+ *        create new thread for it.
+ * @param fp_time_info    The information about the current time.
+ */
+void monitor_time(const struct tm *fp_time_info)
+{
+    //! Hold the previous second.
+    static int prev_sec = 0;
+    int        index    = 0;
+
+    if (fp_time_info == NULL)
+    {
+        printf("fp_time_info is NULL\n");
+
+        exit(1);
+    }
+    else
+    {
+        //! In order to check scripts once every second.
+        if (prev_sec != fp_time_info->tm_sec)
+        {
+            printf("Hour %d, min %d, sec %d\n", fp_time_info->tm_hour, fp_time_info->tm_min, fp_time_info->tm_sec);
+
+            //! Through every available script.
+            for(index = 0; index < fs_count_of_scripts_for_execution; index++)
+            {
+                //! Check the time of execution.
+                if ((fs_scripts_for_execution[index].hour   == fp_time_info->tm_hour) &&
+                    (fs_scripts_for_execution[index].minute == fp_time_info->tm_min) &&
+                    (fs_scripts_for_execution[index].second == fp_time_info->tm_sec))
+                {
+                    //! Create the new thread where the script will be executed.
+                    create_new_thread(fs_scripts_for_execution[index].script_name,
+                                      fs_scripts_for_execution[index].pthread_id);
+                }
+            }
+
+            //! Store current second as previous
+            prev_sec = fp_time_info->tm_sec;
+        }
+    }
+}
---
+++ Versions/Script_Caller_V1.1_Working/scripts/backup_monitor.py
@@ -0,0 +1,77 @@
+'''
+Created on Jun 9, 2017
+
+@author: lalov
+'''
+
+import os
+import filecmp
+from shutil import copyfile
+
+waking_all_disks_command =  "fdisk -l"
+
+mirrorFrom = ["/home/lalov/Pictures",
+              "/home/lalov/Documents",
+              "/home/lalov/Music"]
+mirrorTo   = ["/run/media/lalov/Gogo's Disk/Lalov/Pics",
+              "/run/media/lalov/Gogo's Disk/Lalov/Documents",
+              "/run/media/lalov/Gogo's Disk/Lalov/Music"]
+
+index = 0
+
+def is_file_exist(fileName):
+    return os.path.isfile(fileName)  
+
+def is_dir_exist(directory):
+    return os.path.isdir(directory)
+
+def get_destination_file_full_path(root, fileName):
+    base_path_lenght = len(mirrorFrom[index])
+    
+    return mirrorTo[index] + root[base_path_lenght:] + '/' + fileName
+
+def get_source_file_full_path(root, fileName):
+    return root + '/' + fileName
+
+def get_destination_dir_full_path(root, ___dir):
+    base_path_lenght = len(mirrorFrom[index])
+
+    return mirrorTo[index] + root[base_path_lenght:] + '/' + ___dir
+
+# True if they seem equal, False otherwise
+def are_files_equal(file1, file2):
+    return filecmp.cmp(file1, file2)
+
+def recursive_walk(source_dir):
+    for root, dirs, files in os.walk(source_dir):
+        for ___dir in dirs:
+            destination_dir = get_destination_dir_full_path(root, ___dir)
+            
+            if (is_dir_exist(destination_dir)):
+                pass
+            else:
+                os.makedirs(destination_dir)
+
+        for file in files:
+            destination_path = get_destination_file_full_path(root, file)
+            source_path      = get_source_file_full_path(root, file)
+            
+            if (is_file_exist(destination_path)):
+                if (are_files_equal(source_path, destination_path)):
+                    pass
+                else:
+                    copyfile(source_path, destination_path)
+                    print ('coped from ' + source_path + ' to ' + destination_path)
+            else:
+                copyfile(source_path, destination_path)
+                print ('coped from ' + source_path + ' to ' + destination_path)
+
+if __name__ == '__main__':
+    os.system(waking_all_disks_command)
+
+    while (index < len(mirrorFrom)):
+        recursive_walk(mirrorFrom[index])
+        index += 1
+           
+    print ('Done')
+
